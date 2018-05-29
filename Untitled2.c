d0c1278873d51b70efb423a6b9f7d4e88d8c919a
unknown <ten.arrow.wind@gmail.com>
2018-05-30 01:41:29 +0800

    first commit

---
+++ Untitled2.c
@@ -0,0 +1,14 @@
+void bubbleSort(int *a){
+	int i,j,temp;
+	for(i=0;i<5;i++) {
+		for(j=0;j<4;j++){
+			
+		if (a[j]>a[j+1]){
+			temp=a[j];
+			a[j]=a[j+1];
+			a[j+1]=temp;
+		}
+	}
+	}
+	
+}
---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+void bubbleSort(int *a);
+int main(int argc, char *argv[]) {
+	int a[5]={1,6,9,8,4},i;
+	bubbleSort(a);
+	for(i=0;i<5;i++){
+		printf (" %d",a[i]);
+	}
+	
+	return 0;
+}
